c647bbc09e516dd314a9b7270c6db7a1c40f4dba
Reed Essick <reed@rbe4.org>
2015-12-31 07:00:22 +0000

    started preliminary work to see if I can get mplayer to play music via Python's subprocess. Also put in some basic structure to the executable and config file, etc

--- lib/annoySalvo.py
+++ lib/annoySalvo.py
@@ -3,4 +3,10 @@ author = "Reed Essick"
 
 from RPi import GPIO as gpio
 
+#=================================================
 
+def mplayer_cmd( song ):
+    """
+    defines a command string to launch mplayer
+    """
+    return "mplayer %s"%(song)
