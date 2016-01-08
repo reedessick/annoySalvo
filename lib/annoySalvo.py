be40672a245e5f7b3d94894d5eff34299f6823e1
Reed Essick <reed.@rbe4.org>
2016-01-08 01:11:59 -0500

    debugged gpio issues and reached a working state for the code! However, the audio out through the 3.5mm jack has a LOT of noise and we may want to configure that differently. Can we drive a speaker via the GPIO or something similar?

--- lib/annoySalvo.py
+++ lib/annoySalvo.py
@@ -7,7 +7,7 @@ from RPi import GPIO as gpio
 
 #=================================================
 
-GPIO.setmode(GPIO.BCM) ### set up GPIO using BCM numbering
+gpio.setmode(gpio.BCM) ### set up GPIO using BCM numbering
 
 #=================================================
 
@@ -15,20 +15,23 @@ def mplayer_cmd( song ):
     """
     defines a command string to launch mplayer
     """
-    return "mplayer %s"%(song)
+    return "mplayer -noconsolecontrols %s"%(song)
 
 #=================================================
 
-def has_moved( pin ):
+def has_moved( pin, verbose=True ):
     """
-    determines whether Salvo has moved
-
-    WARNING: currently just returns True
+    determines whether motion was detected via pin
     """
-    return GPIO.input(pin): ### nonzero
+    input = gpio.input( pin )
+    if verbose:
+        print "pin=%d : %.3f"%(pin, input)
+    return input ### positive -> motion detected
 
 def wait():
     """
     determines how long we have to wait until we're allowed to mess with Salvo
+
+    WARNING: currently just returns 0. We need to set up date-time logic to make this useful
     """
     return 0
